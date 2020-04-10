4c1cf44ff5c407e7690007e90976f81364f848ae
HaroldEnrique <haroldcotacallapa@upeu.edu.pe>
2020-04-10 04:42:56 -0500

    first commit before deploy

---
+++ README.md
@@ -0,0 +1,11 @@
+## Minimal Project using Google Drive API
+
+### Functionalities
+- Upload files
+- Create folders
+- Give/Revoke permissions
+- Copy and move files
+
+* That's it and have fun
+
+- @HaroldEnrique
---
+++ values.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+config = {
+    "base_folder_report":"1Ymxz2u5MF0w6nUMkNq1CLCcv_NNquqOu", # folder INFORMES-CUARENTENA
+    "base_folder_spreadsheet":"1P-ki5pZA71jwyVZX8H7kPVIj52BqNtMd",
+    "teachers": [
+        {
+            "id_basic_file":"11rG7RVcyn2gZK4eZdByl5_Ei21hBPBEi",
+            "name": "Harold_Samaria",
+            "email": "harolcotac@gmail.com"
+        },
+        {
+            "id_basic_file":"16mkPkkEDpPs3So1A-QZxIlPWIppuKsLh",
+            "name": "Ailen_Belen",
+            "email": "iacm268@gmail.com"
+        }
+    ],
+    "scopes": ['https://www.googleapis.com/auth/drive.metadata.readonly',
+               'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file']
+}
